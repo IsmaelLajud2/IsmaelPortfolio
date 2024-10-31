57ccbdabc248e22ed610e7f58ab5bae28e11bc01
Ismael <ismael.lajud65@gmail.com>
2024-10-31 11:17:38 +0100

    porfolio Ismael Lajud

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,25 +1,26 @@
 import { useRef } from 'react'
 import '../parallax/ParallaxStyles.css'
-import { motion ,useScroll,useTransform } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
+
 const Parallax = () => {
 
-const ref=useRef(null)
- const {scrollYProgress} =useScroll({
-    target:ref,
-    offset:["start start","end start"]
- })   
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"]
+  })
 
 
- const yText =useTransform(scrollYProgress,[0,1],["0%","500%"])
- const yBG =useTransform(scrollYProgress,[0,1],["0%","100%"])
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"])
+  const yBG = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
 
   return (
     <div className='parallax-container' ref={ref}>
-        <motion.h1 style={{y:yText}} className='parallax-title'> Proyectos</motion.h1>
-            <motion.div  className='mountains'></motion.div>
-            <motion.div style={{y:yBG}} className='planets'></motion.div>
-            <motion.div style={{x:yBG}} className='stars'></motion.div>
-        
+      <motion.h1 style={{ y: yText }}  className='parallax-title'> Proyectos</motion.h1>
+      <motion.div className='mountains'  ></motion.div>
+      <motion.div style={{ y: yBG }} className='planets'></motion.div>
+      <motion.div style={{ x: yBG }} className='stars'></motion.div>
+
     </div>
   )
 }
